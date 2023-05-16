0ba45621dd0bd7269698b8b0ffffcf9893a642f5
ekoi <eko.indarto@dans.knaw.nl>
2023-05-16 12:34:11 +0200

    Initial working version

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM python:3.11.1-slim-bullseye
+
+ARG VERSION=0.1.5
+
+RUN useradd -ms /bin/bash dans
+
+USER dans
+WORKDIR /home/dans
+ENV PYTHONPATH=/home/dans/type-registry-service/src
+ENV BASE_DIR=/home/dans/type-registry-service
+
+COPY ./dist/*.* .
+
+#
+RUN mkdir -p ${BASE_DIR}
+
+COPY ./dist/*.* .
+
+#
+RUN mkdir -p ${BASE_DIR}&& \
+    pip install --no-cache-dir *.whl && rm -rf *.whl && \
+    tar xf type_registry_service-${VERSION}.tar.gz -C ${BASE_DIR} --strip-components 1
+
+WORKDIR ${BASE_DIR}
+
+
+CMD ["python", "src/main.py"]
+#CMD ["tail", "-f", "/dev/null"]
---
+++ src/main.py
@@ -0,0 +1,99 @@
+import importlib.metadata
+import logging
+
+import requests
+import uvicorn
+from dynaconf import Dynaconf
+from fastapi import FastAPI
+from jsonpath_ng.ext import parse
+
+__version__ = importlib.metadata.metadata("type-registry-service")["version"]
+
+settings = Dynaconf(settings_files=["conf/settings.toml"],
+                    environments=True)
+logging.basicConfig(filename=settings.LOG_FILE, level=settings.LOG_LEVEL,
+                    format=settings.LOG_FORMAT)
+
+app = FastAPI(title=settings.FASTAPI_TITLE, description=settings.FASTAPI_DESCRIPTION,
+              version=__version__)
+
+data = {}
+
+
+@app.on_event('startup')
+def common_data():
+    logging.debug("startup")
+
+    url_resp = requests.get(settings.DANS_FORMATS_URL)
+    if url_resp.status_code == 200:
+        data.update({'dans_formats': url_resp.json()})
+        return data
+    # TODO: error or exit?
+    exit(1)
+
+
+@app.get('/')
+def info():
+    logging.info("Type registry service")
+    logging.debug("info")
+    return {"name": "Type registry service", "version": __version__}
+
+
+@app.get('/type/{filetype}')
+def check_type(filetype: str):
+    logging.info("Type registry service")
+    logging.debug("info")
+    dans_formats = data['dans_formats']
+    jsonpath_exp = parse("$..format[*].file-extension")
+
+    for match in jsonpath_exp.find(dans_formats):
+        if match.value == filetype:
+            logging.debug(f'Filetype match: {match.value}')
+            return {"checked": filetype, "accepted": True}
+            break
+
+    return {"checked": filetype, "accepted": False}
+
+
+@app.get('/type-list-simple')
+def retrieve_simple_list():
+    logging.info("Type registry service")
+    logging.debug("info")
+    dans_formats = data['dans_formats']
+    jsonpath_exp = parse("$..format[*].file-extension")
+    list_type = []
+    for match in jsonpath_exp.find(dans_formats):
+        list_type.append(match.value)
+
+    return {"list": list_type}
+
+
+@app.get('/type-list-grouped')
+def retrieve_grouped_list():
+    logging.info("Type registry service")
+    logging.debug("info")
+    dans_formats = data['dans_formats']
+    jsonpath_exp = parse("$..type[*]")
+    list_type = []
+    for match in jsonpath_exp.find(dans_formats):
+        list_extension = []
+        for file_ext in match.context.context.value['format']:
+            list_extension.append(file_ext['file-extension'])
+
+        list_type.append({match.value: list_extension})
+
+    return {"type": list_type}
+
+
+@app.get('/dans-formats')
+def retrieve_dans_formats():
+    logging.info("Type registry service")
+    logging.debug("info")
+
+    return data['dans_formats']
+
+
+if __name__ == "__main__":
+    logging.info("Start")
+
+    uvicorn.run("src.main:app", host="0.0.0.0", port=2023, reload=False)
